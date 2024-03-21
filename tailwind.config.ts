ffdaec835fdc27e74c617e944bbf16ec71c9dc5b
RossG789 <gossrray@gmail.com>
2024-03-21 10:22:42 +0000

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "**",
+          },
+        ],
+      },
     },
   },
   plugins: [],
