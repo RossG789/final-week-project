b8809d6578a8ccc3e682b2b141cd506b0c5d391e
Scrobwofl <scrobwofl@gmail.com>
2024-03-26 13:04:50 +0000

    Add like functionality

--- src/app/components/HandleDislike.tsx
+++ src/app/components/HandleDislike.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { auth } from "@clerk/nextjs";
 import { db } from "@/db";
 
-const userId = auth().userId;
+const { userId } = auth();
 
 interface Business {
   id: string;
@@ -15,7 +15,33 @@ interface Business {
 
 export default async function HandleDislike(business: Business) {
   console.log(`Disliked ${business.name}`);
-  // Remove from page
-  // Add it to disliked database
-  // Add animation?
+  try {
+    await db.query(
+      `
+     DELETE FROM likes
+     WHERE restaurant_id = $1 AND users_id = $2
+ `,
+      [business.id, userId]
+    );
+    await db.query(
+      `
+      INSERT INTO dislikes (restaurant_id, users_id) VALUES ($1, $2)`,
+      [business.id, userId]
+    );
+    //    await db.query(
+    //      `
+    //      DELETE FROM restaurants
+    //      WHERE restaurant_id = $1
+    // `,
+    // [business.id]
+    // );
+
+    console.log("record deleted successfully");
+  } catch (error) {
+    console.error("Error deleting record:", error);
+    throw error;
+  }
 }
+// Remove from page
+// Add it to disliked database
+// Add animation?
--- src/app/components/HandleLike.tsx
+++ src/app/components/HandleLike.tsx
@@ -13,15 +13,19 @@ interface Business {
 
 export default async function HandleLike(business: Business) {
   console.log(`Liked ${business.name}`);
-
-  await db.query(
-    `INSERT INTO restaurants (restaurant_id, name,img_url) VALUES ($1, $2, $3)`,
-    [business.id, business.name, business.image_url]
-  );
-  await db.query(
-    `INSERT INTO likes (restaurant_id, users_id) VALUES ($1, $2)`,
-    [business.id, userId]
-  );
+  try {
+    await db.query(
+      `INSERT INTO restaurants (restaurant_id, name,img_url) VALUES ($1, $2, $3)`,
+      [business.id, business.name, business.image_url]
+    );
+    await db.query(
+      `INSERT INTO likes (restaurant_id, users_id) VALUES ($1, $2)`,
+      [business.id, userId]
+    );
+  } catch (error) {
+    console.error("Error deleting book:", error);
+    throw error;
+  }
   // Remove from page
   // Add it to liked database
   // Add animation?
